8d04ac54fd53a3f288040e266567804f46d5be85
hieunugent <hieunugent@gmail.com>
2024-09-01 09:48:04 -0700

    update

---
+++ users/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegisterForm
+
+
+# Create your views here.
+def register(request):
+    if request.method =='POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user =form.save()
+            login(request,user)
+            messages.success(request,'Your account has been created successfully!')
+            return redirect('home')
+    else:
+        form =UserRegisterForm()
+    return render(request,'users/register.html', {'form':form})
