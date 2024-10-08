554d210b30f54d715b519eccc5bbca5d9fb440b0
Henry Hieu Nguyen <hieunugent@gmail.com>
2024-10-08 13:47:05 -0700

    fix vercel.json and api.index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from django.core.wsgi import get_wsgi_application
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanagerment.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmanagement.settings')
 app = get_wsgi_application()
